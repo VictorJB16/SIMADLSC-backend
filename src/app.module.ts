1241027f130879e6d80d340ebcdd1dc59da22577
Cristian <Martinguaduz@gmail,com>
2024-09-12 23:02:25 -0600

    Eliminado modulo usuario y auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { AsistenciaModule } from './asistencia/asistencia.module';
-import { UsuarioModule } from './usuario/usuario.module';
+
 
 @Module({
-  imports: [AuthModule, AsistenciaModule, UsuarioModule],
+  imports: [ AsistenciaModule],
   controllers: [AppController],
   providers: [AppService],
 })
