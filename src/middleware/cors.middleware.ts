52cafa5c7f3c3ec69857b251be827f8d510c8083
Geraldsamurai3 <andreylanza3@gmail.com>
2024-11-10 16:17:33 -0600

    url actualizadas 6

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173 , https://simadlsc-lscc.vercel.app/');  // Permitir solicitudes desde cualquier origen
+    res.header('Access-Control-Allow-Origin','https://simadlsc-lscc.vercel.app/');  // Permitir solicitudes desde cualquier origen
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,REJECT,APPROVE');  // Métodos permitidos
     res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');  // Cabeceras permitidas
     next();
