a9052f2b41335d773c5a866ffdd24831839b8449
Geraldsamurai3 <andreylanza3@gmail.com>
2024-11-10 22:21:15 -0600

    quitar middleware cors

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,32 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { whitelist } from 'validator';
 import * as dotenv from 'dotenv';
 import { AllExceptionsFilter } from './horario/filter/AllExceptionsFilter';
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new AllExceptionsFilter);
+
+  // Configurar filtros globales y excepciones
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  // Configurar CORS
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://simadlsc-lscc.vercel.app/'],
-    methods: 'GET,POST,PUT,DELETE,OPTIONS,PATCH,REJECT,APPROVE',
-    allowedHeaders: 'Content-Type, Authorization',
-    credentials: true, 
+    origin: ['http://localhost:5173', 'https://simadlsc-lscc.vercel.app'], // sin la barra final
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Opciones permitidas
+    allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+    credentials: true,
   });
-  
+
+  // Configurar validaciones globales
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, 
-    forbidNonWhitelisted: true, 
-    transform: true, 
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
   }));
-  await app.listen(3000);
+
+  await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
