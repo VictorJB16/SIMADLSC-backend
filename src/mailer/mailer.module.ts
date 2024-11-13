4d801e09483ac7a9af6cb26bda686055d832a148
Cristian <Martinguaduz@gmail,com>
2024-11-12 21:02:57 -0600

    Mailer 2

--- src/mailer/mailer.module.ts
+++ src/mailer/mailer.module.ts
@@ -2,38 +2,34 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { join } from 'path';
-import { ConfigModule, ConfigService } from '@nestjs/config'; // Importa ConfigModule y ConfigService
 import { MailerCustomService } from './mailer.service';
 
 @Module({
   imports: [
-    MailerModule.forRootAsync({
-      imports: [ConfigModule], // Importa ConfigModule aquí
-      inject: [ConfigService],
-      useFactory: async (config: ConfigService) => ({
-        transport: {
-          host: config.get<string>('SMTP_HOST'),
-          port: config.get<number>('SMTP_PORT'),
-          secure: config.get<boolean>('SMTP_SECURE') === true,
-          auth: {
-            user: config.get<string>('SMTP_USER'),
-            pass: config.get<string>('SMTP_PASS'),
-          },
-          tls: {
-            rejectUnauthorized: false, // Opcional, útil para pruebas
-          },
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com', // Servidor SMTP
+        port: 587, // Puerto de Gmail para TLS
+        secure: false, // `false` para usar STARTTLS en el puerto 587
+        auth: {
+          user: 'andreylanza3@gmail.com', // Reemplaza con tu correo
+          pass: 'nlil hjpl cntp bauq', // Reemplaza con la contraseña de aplicación de Gmail
         },
-        defaults: {
-          from: '"No Reply" <noreply@example.com>',
+        tls: {
+          rejectUnauthorized: false, // Útil para pruebas; asegúrate de cambiarlo en producción si es necesario
         },
-        template: {
-          dir: join(process.cwd(), 'src', 'mailer', 'templates'),
-          adapter: new HandlebarsAdapter(),
-          options: {
-            strict: true,
-          },
+        family: 4, // Usar IPv4, puede ayudar a evitar problemas de conectividad con IPv6
+      },
+      defaults: {
+        from: '"No Reply" <noreply@example.com>', // Correo y nombre por defecto del remitente
+      },
+      template: {
+        dir: join(process.cwd(), 'src', 'mailer', 'templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
         },
-      }),
+      },
     }),
   ],
   providers: [MailerCustomService],
