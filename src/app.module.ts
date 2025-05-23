7eb929a142c37b123b6f1626434559f9eed5dc10
Geraldsamurai3 <andreylanza3@gmail.com>
2025-05-23 01:51:54 -0600

    cambio de despliegue

--- src/app.module.ts
+++ src/app.module.ts
@@ -39,7 +39,7 @@ const config = () => ({
   database: {
     url:
       process.env.DATABASE_URL ||
-      'mariadb://railway:Yiieg23i2eNM~vR2CGnTT658J_usWTo.@ballast.proxy.rlwy.net:19186/simadlsc',
+      'mariadb://railway:7vveQiTcqYSmc7on7.O.UomcGXsBLsX.@ballast.proxy.rlwy.net:22790/railway',
   },
   mail: {
     host: process.env.SMTP_HOST || 'smtp.gmail.com',
